20c712ec2b337789d3e35c5db01750393205b92e
Enoch Hsiao <enochhsiao@hotmail.com>
2020-09-22 16:36:46 -0400

    Added react router (routes to login page). Deleted boilerplate code and files from create-react-app. Added preliminary login page.

---
+++ client_folder_react_app/ethisim/src/pages/login.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { 
+  Button, 
+  TextField, 
+  FormControlLabel, 
+  Checkbox, 
+  Box, 
+  Typography, 
+  Container,
+} from '@material-ui/core';
+import Copyright from '../components/copyright'
+
+export default function SignIn() {
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      marginTop: theme.spacing(6),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    logo: {
+      //TODO Implement logo styling
+    },
+    form: {
+      marginTop: theme.spacing(1),
+      width: '100%', 
+    },
+    submit: {
+      marginTop: theme.spacing(2),
+      backgroundColor:  '#881c1c',
+      color: "white"
+    },
+    copyright: {
+      marginTop: theme.spacing(2),
+    }
+  }));
+
+  const classes = useStyles();
+
+  //TODO add in Ethisim Logo at top of page
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className={classes.container}>
+
+        <Typography variant="h4">
+          Login
+        </Typography>
+        <form className={classes.form}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary"/>}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+        </form>
+      </div>
+      <Box className={classes.copyright}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
