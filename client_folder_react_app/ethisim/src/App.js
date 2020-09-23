9e4eec45bf33e00cb2d3173b968319c5c977e216
Enea Dodi <eneadodi@umass.edu>
2020-09-23 11:23:15 -0400

    Register and Login Page set up. Link from Register to Login setup

--- client_folder_react_app/ethisim/src/App.js
+++ client_folder_react_app/ethisim/src/App.js
@@ -7,13 +7,15 @@ import {
   Redirect,
 } from "react-router-dom";
 import Register from './pages/register_account';
-
+import Login from './pages/login';
 export default function App() {
   return (
     <Router>
         <Route exact path="/">
           <Redirect to="/register_account" />
         </Route>
+
+        <Route path = "/login" component={Login}/>
 
         <Switch>
           <Route path="/register_account">
