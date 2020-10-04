238e352faaf2d42d54f5a5d9ec4a9684b5e51267
Enoch Hsiao <enochhsiao@hotmail.com>
2020-10-04 14:00:02 -0400

    sidebar implementation

---
+++ Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/scenario_components/ConversationEditor.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default function ConversationEditor() {
+  return (
+    <Typography align="center">
+      Conversation Editor Component
+    </Typography>
+  );
+}
---
+++ Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/scenario_components/Event.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default function Introduction() {
+  return (
+    <Typography align="center">
+      Event Component
+    </Typography>
+  );
+}
