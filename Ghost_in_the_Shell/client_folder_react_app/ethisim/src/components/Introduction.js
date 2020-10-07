466c338a0a98de41cd715615be5b58493277d197
Enoch Hsiao <enochhsiao@hotmail.com>
2020-10-07 15:56:06 -0400

    responsive buttons for text editor

--- Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/Introduction.js
+++ Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/Introduction.js
@@ -35,7 +35,29 @@ export default function Introduction() {
             ['table', 'link', 'image', 'video', 'audio'], 
             ['fullScreen', 'showBlocks', 'codeView'],
             ['preview',],
-          ]
+            // (min-width: 1000px)
+            ['%1000', [
+              ['undo', 'redo'],
+              [':p-More Paragraph-default.more_paragraph', 'font', 'fontSize', 'formatBlock', 'paragraphStyle', 'blockquote'],
+              ['bold', 'underline', 'italic', 'strike'],
+              [':t-More Text-default.more_text', 'subscript', 'superscript', 'fontColor', 'hiliteColor', 'textStyle'],
+              ['removeFormat'],
+              ['outdent', 'indent'],
+              [':e-More Line-default.more_horizontal', 'align', 'horizontalRule', 'list', 'lineHeight'],
+              ['-right', ':i-More Misc-default.more_vertical', 'fullScreen', 'showBlocks', 'codeView', 'preview'],
+              ['-right', ':r-More Rich-default.more_plus', 'table', 'link', 'image', 'video', 'audio'],
+            ]],
+            // (min-width: 875px)
+            ['%875', [
+              ['undo', 'redo'],
+              [':p-More Paragraph-default.more_paragraph', 'font', 'fontSize', 'formatBlock', 'paragraphStyle', 'blockquote'],
+              [':t-More Text-default.more_text', 'bold', 'underline', 'italic', 'strike', 'subscript', 'superscript', 'fontColor', 'hiliteColor', 'textStyle', 'removeFormat'],
+              [':e-More Line-default.more_horizontal', 'outdent', 'indent', 'align', 'horizontalRule', 'list', 'lineHeight'],
+              [':r-More Rich-default.more_plus', 'table', 'link', 'image', 'video', 'audio'],
+              ['-right', ':i-More Misc-default.more_vertical', 'fullScreen', 'showBlocks', 'codeView', 'preview']
+            ]],
+          ],
+          
         }}
         onChange={handleChange}
       />
