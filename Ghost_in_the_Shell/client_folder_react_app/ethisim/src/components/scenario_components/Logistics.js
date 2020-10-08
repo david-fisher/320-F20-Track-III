bf0ec9e0d987aebb5bc022bd2dcbba1bb7187e92
Tyler <tygriffin115@gmail.com>
2020-10-07 21:39:37 -0400

    Removed Unused Imports

--- Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/scenario_components/Logistics.js
+++ Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/scenario_components/Logistics.js
@@ -3,10 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Button';
-import Box from '@material-ui/core/Button';
-import View from '@material-ui/core/Button';
-import Copyright from '../Copyright';
 
 const useStyles = makeStyles((theme) => ({
   textfields: {
