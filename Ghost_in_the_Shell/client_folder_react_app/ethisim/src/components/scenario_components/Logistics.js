e5515b36e368a735d9e093d9d168c02183d766e1
benjaminxu251 <39343316+benjaminxu251@users.noreply.github.com>
2020-10-12 14:31:58 -0400

    made the "add author" button look nicer

--- Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/scenario_components/Logistics.js
+++ Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/scenario_components/Logistics.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
@@ -12,10 +12,10 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   buttons: {
-    '& > *':{
-    margin: theme.spacing(1),
-    width: '100%',
-  },
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '100%',
+    },
   },
   subdiv: {
     marginTop: theme.spacing(1),
@@ -37,39 +37,39 @@ export default function Logistics() {
 
   return (
     <div>
-          <Typography align="center" variant="h2">
-            Logistics
+      <Typography align="center" variant="h2">
+        Logistics
           </Typography >
-            <Typography align = "left" variant="h6">
-              <form className={classes.textfields} noValidate autoComplete="off">
-                Simulation Title
+      <Typography align="left" variant="h6">
+        <form className={classes.textfields} noValidate autoComplete="off">
+          Simulation Title
               <TextField id="Simulation Title" label="" />
                 Course Name
               <TextField id="Course Name" label="" />
                 Authors
               {authors}
-              <button onClick={addAuthor}>
-                Add Author
-              </button>
+          <Button variant="contained" color="primary" onClick={addAuthor}>
+            Add Author
+              </Button>
               Scenario ID:
               1342431
               </form>
               Shareable Link:
               wwww.ethisim.com
             </Typography>
-          <div className = {classes.subdiv}>
-            <form className={classes.buttons} noValidate autoComplete="off">
-              <Button variant="contained" color="grey" >
-               View Student Responses
+      <div className={classes.subdiv}>
+        <form className={classes.buttons} noValidate autoComplete="off">
+          <Button variant="contained" color="grey" >
+            View Student Responses
               </Button>
-              <Button variant="contained" color="primary" >
-              Delete Scenario
+          <Button variant="contained" color="primary" >
+            Delete Scenario
               </Button>
-              <Button variant="contained" color="primary" >
-              View Version History
+          <Button variant="contained" color="primary" >
+            View Version History
               </Button>
-            </form>
-          </div>
+        </form>
+      </div>
     </div>
   );
 }
