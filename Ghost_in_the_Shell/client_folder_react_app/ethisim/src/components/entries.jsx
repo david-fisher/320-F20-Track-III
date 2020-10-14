09fc64f5655085a55d490cbf4f7234ef4b3ee990
Jaivardhan1 <70129874+Jaivardhan1@users.noreply.github.com>
2020-10-15 05:11:53 +0530

    Configure Issues Entry Fields v2

---
+++ Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/entries.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import EntryField from "./entry";
+import Button from "@material-ui/core/Button";
+
+class EntryFields extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      entries: [],
+      entryCur: {
+        key: " ",
+        id: 0,
+      },
+    };
+    this.addItem = this.addItem.bind(this);
+  }
+
+  handleDelete = (entryID) => {
+    const entries = this.state.entries.filter((q) => q.id !== entryID);
+    this.setState({ entries });
+  };
+
+  addItem(e) {
+    e.preventDefault();
+    const newEntry = this.state.entryCur;
+    const newEntries = [...this.state.entries, newEntry];
+
+    this.setState({
+      entries: newEntries,
+      entryCur: {
+        key: " ",
+        id: this.state.entryCur.id + 1,
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div className="entries">
+        <Button
+          id="button"
+          onClick={this.addItem}
+          variant="contained"
+          color="primary"
+        >
+          Create Issue
+        </Button>
+
+        <form id="form">
+          {this.state.entries.map((entry) => (
+            <EntryField
+              key={entry.id}
+              onDelete={this.handleDelete}
+              entry={entry}
+            />
+          ))}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default EntryFields;
