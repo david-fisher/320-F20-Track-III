51ad79ce599469abff7307ad8138149bf1160e84
sixbrigands <dcalhq@gmail.com>
2020-10-14 21:31:10 -0400

    test 1 again

--- moral_kombat_backend/lead/home/api.py
+++ moral_kombat_backend/lead/home/api.py
@@ -1,9 +1,3 @@
 from rest_framework import viewsets, permissions
-from .serializer import 
+from .serializer import DemographicsSerializer, StudentSerializer
 
-class TablesViewSet(viewsets.ModelViewSet):
-    queryset = Tables.objects.all()
-    permission_classes = [
-        permissions.AllowAny
-    ]
-    serializer_class = HomeSerializer
