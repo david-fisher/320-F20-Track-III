021a25de1a6fc85edb8ecb76093199175c78fdab
benjaminxu251 <39343316+benjaminxu251@users.noreply.github.com>
2020-10-18 19:40:13 -0400

    importaed and used files needed for 'isMobile', which returns true if the user is using a phone or tablet.

--- Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/scenario_components/Logistics.js
+++ Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/scenario_components/Logistics.js
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Author from './Author';
+import { isMobile, isBrowser } from "react-device-detect";
 
 const useStyles = makeStyles((theme) => ({
   textfields: {
@@ -36,48 +37,101 @@ export default function Logistics() {
     event.preventDefault()
   }
 
-  return (
+  //default if it's a browser
+  var body = (
     <div>
       <Typography align="center" variant="h2">
         Logistics
-          </Typography >
+              </Typography >
       <Typography align="left" variant="h6">
         <form className={classes.textfields} noValidate autoComplete="off">
           Simulation Title
-              <TextField id="Simulation Title" label="" />
-                Course Name
-              <TextField id="Course Name" label="" />
-                Authors
-              {authors}
+                  <TextField id="Simulation Title" label="" />
+                    Course Name
+                  <TextField id="Course Name" label="" />
+                    Authors
+                  {authors}
           <Button variant="contained" color="primary" onClick={addAuthor}>
             Add Author
-              </Button>
+                  </Button>
           <Button
             className={classes.margin}
             variant="contained"
             color="primary"
           >
             Save Authors
-            </Button>
-              Scenario ID:
-              1342431
-              </form>
-              Shareable Link:
-              wwww.ethisim.com
-            </Typography>
+                </Button>
+                  Scenario ID:
+                  1342431
+                  </form>
+                  Shareable Link:
+                  wwww.ethisim.com
+                </Typography>
       <div className={classes.subdiv}>
         <form className={classes.buttons} noValidate autoComplete="off">
           <Button variant="contained" color="grey" >
             View Student Responses
-              </Button>
+                  </Button>
           <Button variant="contained" color="primary" >
             Delete Scenario
-              </Button>
+                  </Button>
           <Button variant="contained" color="primary" >
             View Version History
-              </Button>
+                  </Button>
         </form>
       </div>
     </div>
+  );
+
+  //convert this to "isMobile" later; using "isBrowser" for testing purposes
+  if (isBrowser) {
+    body = (
+      <div>
+        <Typography align="center" variant="h2">
+          Logistics
+              </Typography >
+        <Typography align="left" variant="h6">
+          <form className={classes.textfields} noValidate autoComplete="off">
+            Simulation Title
+                  <TextField id="Simulation Title" label="" />
+                    Course Name
+                  <TextField id="Course Name" label="" />
+                    Authors
+                  {authors}
+            <Button variant="contained" color="primary" onClick={addAuthor}>
+              Add Author
+                  </Button>
+            <Button
+              className={classes.margin}
+              variant="contained"
+              color="primary"
+            >
+              Save Authors
+                </Button>
+                  Scenario ID:
+                  1342431
+                  </form>
+                  Shareable Link:
+                  wwww.ethisim.com
+                </Typography>
+        <div className={classes.subdiv}>
+          <form className={classes.buttons} noValidate autoComplete="off">
+            <Button variant="contained" color="grey" >
+              View Student Responses
+                  </Button>
+            <Button variant="contained" color="primary" >
+              Delete Scenario
+                  </Button>
+            <Button variant="contained" color="primary" >
+              View Version History
+                  </Button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    body
   );
 }
