c569f33a7ee4a86348da114dfaa7e64c179285f5
benjaminxu251 <39343316+benjaminxu251@users.noreply.github.com>
2020-10-19 15:53:22 -0400

    removed two unused imports

--- Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/scenario_components/InformationItem.js
+++ Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/scenario_components/InformationItem.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
 import { Button } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 import SunEditor from "suneditor-react";
