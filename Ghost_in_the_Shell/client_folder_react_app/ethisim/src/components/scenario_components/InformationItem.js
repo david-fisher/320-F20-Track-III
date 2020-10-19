75c344c4d20ad5f8f778a0e57cc19914dd604727
benjaminxu251 <39343316+benjaminxu251@users.noreply.github.com>
2020-10-19 12:13:48 -0400

    added font styling to information item text box (copied and modified from Enoch's branch), made textbox reactive based on browser size (same conditions as Enoch's branch for consistency)

--- Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/scenario_components/InformationItem.js
+++ Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/scenario_components/InformationItem.js
@@ -3,6 +3,9 @@ import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import { Button } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
+import SunEditor from "suneditor-react";
+import 'suneditor/dist/css/suneditor.min.css';
+import htmlToText from 'html-to-text';
 
 const useStyles = makeStyles((theme) => ({
   margin: {
@@ -13,11 +16,61 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+let handleChange = (content) => {
+  //TODO Implement
+  console.log(content);
+  console.log(htmlToText.fromString(content));
+};
+
 export default function InformationItem(props) {
   const classes = useStyles();
 
   return (
     <div>
+      <SunEditor
+        setOptions={{
+          width:'100%',
+          height: '10%',
+          placeholder: "Enter body for information item...",
+          buttonList: [
+            ['font', 'fontSize', 'formatBlock'],
+            ['paragraphStyle', 'blockquote'],
+            ['bold', 'underline', 'italic', 'strike', 'subscript', 'superscript'],
+            ['fontColor', 'hiliteColor', 'textStyle'],
+            '/', // Line break
+            ['undo', 'redo'],
+            ['removeFormat'],
+            ['outdent', 'indent'],
+            ['align', 'horizontalRule', 'list', 'lineHeight'],
+            ['table', 'link', 'image', 'video', 'audio'], 
+            ['fullScreen', 'showBlocks', 'codeView'],
+            ['preview',],
+            // (min-width: 1000px)
+            ['%1000', [
+              ['undo', 'redo'],
+              [':p-More Paragraph-default.more_paragraph', 'font', 'fontSize', 'formatBlock', 'paragraphStyle', 'blockquote'],
+              ['bold', 'underline', 'italic', 'strike'],
+              [':t-More Text-default.more_text', 'subscript', 'superscript', 'fontColor', 'hiliteColor', 'textStyle'],
+              ['removeFormat'],
+              ['outdent', 'indent'],
+              [':e-More Line-default.more_horizontal', 'align', 'horizontalRule', 'list', 'lineHeight'],
+              ['-right', ':i-More Misc-default.more_vertical', 'fullScreen', 'showBlocks', 'codeView', 'preview'],
+              ['-right', ':r-More Rich-default.more_plus', 'table', 'link', 'image', 'video', 'audio'],
+            ]],
+            // (min-width: 875px)
+            ['%875', [
+              ['undo', 'redo'],
+              [':p-More Paragraph-default.more_paragraph', 'font', 'fontSize', 'formatBlock', 'paragraphStyle', 'blockquote'],
+              [':t-More Text-default.more_text', 'bold', 'underline', 'italic', 'strike', 'subscript', 'superscript', 'fontColor', 'hiliteColor', 'textStyle', 'removeFormat'],
+              [':e-More Line-default.more_horizontal', 'outdent', 'indent', 'align', 'horizontalRule', 'list', 'lineHeight'],
+              [':r-More Rich-default.more_plus', 'table', 'link', 'image', 'video', 'audio'],
+              ['-right', ':i-More Misc-default.more_vertical', 'fullScreen', 'showBlocks', 'codeView', 'preview']
+            ]],
+          ],   
+        }}
+        onChange={handleChange}
+      />
+
       <Box
         display="flex"
         flexDirection="row"
@@ -25,6 +78,7 @@ export default function InformationItem(props) {
         m={1}
         bgcolor="background.paper"
       >
+      {/*
         <Box p={1}>
           <form noValidate autoComplete="off">
             <TextField
@@ -37,6 +91,7 @@ export default function InformationItem(props) {
             />
           </form>
         </Box>
+      */}
         <Box p={1}>
           <div>
             <Button
@@ -46,8 +101,6 @@ export default function InformationItem(props) {
             >
               Save
             </Button>
-          </div>
-          <div>
             <Button
               className={classes.margin}
               variant="contained"
