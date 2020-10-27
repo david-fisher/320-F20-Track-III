8241366ca2599e1ff0a8c60082a9005ddc537f26
Enea Dodi <eneadodi@umass.edu>
2020-10-27 13:43:34 -0400

    fixed add author problem

--- Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/scenario_components/Logistics.js
+++ Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/scenario_components/Logistics.js
@@ -46,6 +46,11 @@ export default function Logistics() {
     const [id, setId] = useState(0);
     const [authors, setAuthor] = useState([<Author key={id} />]);
 
+    const addAuthor = (event) => {
+          setAuthor(authors.concat(<Author key={id + 1} />));
+          setId(id + 1);
+          event.preventDefault();
+  };
   //default if it's a browser
   var body = (
     <Container component="main">
@@ -159,69 +164,6 @@ export default function Logistics() {
       </Container>
     );
 
-    //convert this to "isMobile" later; using "isBrowser" for testing purposes
-    if (isBrowser) {
-        body = (
-            <Container component="main">
-                <Typography align="center" variant="h2">
-                    Logistics
-                </Typography>
-                <form
-                    className={classes.textfields}
-                    noValidate
-                    autoComplete="off"
-                >
-                    Simulation Title
-                    <TextField id="Simulation Title" label="" />
-                    Course Name
-                    <TextField id="Course Name" label="" />
-                    Authors
-                </form>
-                {authors}
-                <div className={classes.subdiv}>
-                    <form
-                        className={classes.buttons}
-                        noValidate
-                        autoComplete="off"
-                    >
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={addAuthor}
-                        >
-                            Add Author
-                        </Button>
-                        <Button variant="contained" color="primary">
-                            Save Authors
-                        </Button>
-                    </form>
-                </div>
-                <Typography align="left" variant="h6">
-                    Scenario ID: 1342431
-                </Typography>
-                <Typography align="left" variant="h6">
-                    Shareable Link: wwww.ethisim.com
-                </Typography>
-                <div className={classes.subdiv}>
-                    <form
-                        className={classes.buttons}
-                        noValidate
-                        autoComplete="off"
-                    >
-                        <Button variant="contained">
-                            View Student Responses
-                        </Button>
-                        <Button variant="contained" color="primary">
-                            Delete Scenario
-                        </Button>
-                        <Button variant="contained" color="primary">
-                            View Version History
-                        </Button>
-                    </form>
-                </div>
-            </Container>
-        );
-    }
 
     return (body);
 }}
