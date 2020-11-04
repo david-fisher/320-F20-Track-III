63e36d10ee69546feafb02fd41a918fa96d7eba0
Enoch Hsiao <enochhsiao@hotmail.com>
2020-11-04 13:21:18 -0500

    added imports

--- Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/SavedBanner.js
+++ Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/SavedBanner.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Typography, Fade } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
 const useStyles = makeStyles((theme) => ({
     container: {
