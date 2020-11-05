7595d27da771050448a9605ee60479f5825e5708
Enoch Hsiao <enochhsiao@hotmail.com>
2020-11-04 19:46:42 -0500

    typo-fix

--- Ghost_in_the_Shell/client_folder_react_app/ethisim/src/universalHTTPRequests/post.js
+++ Ghost_in_the_Shell/client_folder_react_app/ethisim/src/universalHTTPRequests/post.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { baseURL } from '../../Constants/Config';
+import { baseURL } from '../Constants/Config';
 // Universal fetch request using axios
 export default function universalPost(
     setResponse,
