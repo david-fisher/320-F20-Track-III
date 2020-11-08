ac590eb10444c1a94bc66bd8b1777c53b65b2aad
Tyler <tygriffin115@gmail.com>
2020-11-08 14:43:22 -0500

    Implemented Persistent Drawer for the Dynamic sidebar
    Took the Persistent Drawer Component from Material-UI and integrated it into our existing code to allow for the sidebar to be open or closed. Also added an AppBar to the top of the scenarioeditor.

--- Ghost_in_the_Shell/client_folder_react_app/ethisim/src/pages/editor.js
+++ Ghost_in_the_Shell/client_folder_react_app/ethisim/src/pages/editor.js
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Drawer, Button, Typography } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import clsx from 'clsx';
+import { useTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Logistics from '../components/EditorComponents/LogisticsPageComponents/Logistics';
 import Generic from '../components/EditorComponents/GenericPageComponents/Generic';
 import ConfigureIssues from '../components/EditorComponents/ConfigureIssuesComponents/ConfigureIssues';
@@ -17,6 +27,20 @@ const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
     },
     menuButton: {
         marginRight: theme.spacing(2),
@@ -38,7 +62,7 @@ const useStyles = makeStyles((theme) => ({
     container: {
         marginTop: theme.spacing(1),
         display: 'flex',
-        flexDirection: 'column',
+        flexDirection: 'row',
         alignItems: 'center',
     },
     title: {
@@ -53,6 +77,30 @@ const useStyles = makeStyles((theme) => ({
         textTransform: 'unset',
         border: 'solid 3px',
         borderColor: theme.palette.primary.light,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
     },
 }));
 
@@ -92,6 +140,15 @@ var startList = [
 
 export default function Editor(props) {
     const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
     const [openPopup, setOpenPopup] = useState(false);
     //Fake fetch of scenarioData with components
     let fetchedComponents = mockUnfinishedScenarioData.components;
@@ -153,12 +210,22 @@ export default function Editor(props) {
             <div>
                 <Drawer
                     className={classes.drawer}
-                    variant="permanent"
+                    variant="persistent"
+                    anchor="left"
+                    open={open}
                     classes={{
                         paper: classes.drawerPaper,
                     }}
-                    anchor="left"
                 >
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? (
+                            <ChevronLeftIcon />
+                        ) : (
+                            <ChevronRightIcon />
+                        )}
+                        Hide Menu
+                    </IconButton>
+
                     <NavSideBarList
                         onClick={onClick}
                         deleteByID={deleteByID}
@@ -185,9 +252,43 @@ export default function Editor(props) {
     }
 
     return (
-        <div className={classes.root}>
+        <div className={classes.container}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(
+                            classes.menuButton,
+                            open && classes.hide
+                        )}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        Ethisim Scenario Editor
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+
             <Sidebar />
-            <main className={classes.content}>{scenarioComponent}</main>
+
+            <main
+                className={clsx(classes.content, {
+                    [classes.contentShift]: open,
+                })}
+            >
+                <div className={classes.drawerHeader} />
+                {scenarioComponent}
+            </main>
         </div>
     );
 }
