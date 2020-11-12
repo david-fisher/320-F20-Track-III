b28e43fb7e54dc84cd05117259ce241569b4dcf1
Jaivardhan1 <jaivardhansi@umass.edu>
2020-11-12 22:07:36 +0530

    version 2 of the Share Button

--- Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/DashboardComponents/ShareButton.js
+++ Ghost_in_the_Shell/client_folder_react_app/ethisim/src/components/DashboardComponents/ShareButton.js
@@ -1,25 +1,33 @@
 import React, { useState } from 'react';
-import { Button, TextField, Typography, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-//import Body from '../GeneralPageComponents/Body';
-//import Title from '../GeneralPageComponents/Title';
+import {
+    Typography,
+    Button,
+    Dialog,
+    DialogActions,
+    TextField,
+    IconButton,
+} from '@material-ui/core';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import CloseIcon from '@material-ui/icons/Close';
+import PropTypes from 'prop-types';
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        marginTop: theme.spacing(2),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+    root: {
+        margin: 0,
+        padding: theme.spacing(1),
+        textAlign: 'center',
     },
-    form: {
-        marginTop: theme.spacing(1),
-        width: '100%',
+    closeButton: {
+        position: 'absolute',
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500],
     },
-    submit: {
-        marginTop: theme.spacing(2),
-        backgroundColor: theme.palette.primary.main,
-        color: 'white',
-        textTransform: 'unset',
+    shareInfo: {
+        borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+        paddingBottom: '6px',
+        marginBottom: '6px',
     },
     saveButton: {
         margin: theme.spacing(2),
@@ -28,30 +36,67 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function FinalAction(props) {
-    const classes = useStyles();
+DialogTitle.propTypes = {
+    onClose: PropTypes.any.isRequired,
+};
 
-    //const titleData = mockActionComponent.title;
-    //const bodyData = mockActionComponent.body;
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
+function DialogTitle(props) {
+    const classes = useStyles();
+    const { onClose } = props;
+    return (
+        <MuiDialogTitle disableTypography className={classes.root}>
+            <Typography variant="h6">Enter the Email Address of the Instructor</Typography>
+            {onClose ? (
+                <IconButton
+                    aria-label="close"
+                    className={classes.closeButton}
+                    onClick={onClose}
+                >
+                    <CloseIcon />
+                </IconButton>
+            ) : null}
+        </MuiDialogTitle>
+    );
+}
+
+export default function ShareDialog(props) {
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+
     const [EnterEmail, setEnterEmail] = useState('');
 
     const onChangeEnterEmail = (event) => {
         setEnterEmail(event.target.value);
     };
 
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
-        <Container component="main">
-            <Typography align="center" variant="h2">
-                Share Results
-            </Typography>
-            <div className={classes.container}>
-                <form className={classes.form}>
-                    <Typography align="center" variant="h6">
-                        Enter Email
-                    </Typography>
-                    <TextField
+        <div>
+            <Button
+                variant="outlined"
+                color="primary"
+                onClick={handleClickOpen}
+            >
+                Share 
+            </Button>
+            <Dialog
+                fullWidth={true}
+                maxWidth="sm"
+                onClose={handleClose}
+                aria-labelledby="customized-dialog-title"
+                open={open}
+            >
+                <DialogTitle onClose={handleClose}></DialogTitle>
+                
+                <DialogActions>
+                <TextField
                         variant="outlined"
                         margin="normal"
                         required
@@ -64,13 +109,14 @@ export default function FinalAction(props) {
                     />
                     <Button
                         className={classes.saveButton}
-                        variant="contained"
+                        autoFocus
                         color="primary"
+                        onClick={handleClose}
                     >
-                        Share
+                        Submit
                     </Button>
-                </form>
-            </div>
-        </Container>
+                </DialogActions>
+            </Dialog>
+        </div>
     );
 }
